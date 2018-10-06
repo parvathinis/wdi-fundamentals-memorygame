d9ab9e2760ceb2e3fef881ab49ab5e823e5113d7
Srinivasa Parvathini <srinivasa_parvathini@glic.com>
2018-10-06 16:14:56 -0400

    JavaScript control flow changes added to the memory card game

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,22 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+var cardsInPlay = [];
+
+var cardOne = cards[0]; // first element
+cardsInPlay.push(cardOne);
+
+console.log("User flipped " + cardOne); 
+
+
+var cardTwo = cards[2]; // third element
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped " + cardTwo); 
+
+
+if(cardsInPlay.length === 2) {
+    if(cardsInPlay[0] === cardsInPlay[1])
+        alert("You found a match!");
+    else
+        alert("Sorry, try again.");
+}
